9a5c827ab2b4f36983fa4b67b39c292f4af1fac3
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-05 08:11:36 +0530

    Added:to toss to check who plays first

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,41 +8,53 @@ public class TicTacToe {
 	static String[] board;
 	static String turn;
 
+	static void populateEmptyBoard() {
+
+         	for (int a = 0; a < 9; a++) {
+                board[a] = String.valueOf(a+1);
+                }
+	}
+
+	static void printBoard() {
+
+                System.out.println("/---|---|---\\");
+                System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+                System.out.println("/---|---|---\\");
+
+        }
+
+
+
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
 		board = new String[9];
-		turn =in.nextLine();
 		String winner = null;
 		populateEmptyBoard();
 
+		//Taking the choice of the user
+		System.out.println("Enter a number between 0 and 1");
+		int choice=in.nextInt();
+		//getting the result of the toss
 		int n=(int) (Math.random() + 0.5);
-		if(n==1) {
+		//checking if the user won the toss or not
+		if(n==choice) {
 		System.out.println("Player will proceed");
-
-		turn="X";
-		}else {
+		}
+		else {
 			System.out.println("Computer will proceed");
-			turn="O";
 		}
 
 		System.out.println("Welcome to 2 Player Tic Tac Toe.");
 		System.out.println("--------------------------------");
 		printBoard();
-		System.out.println(turn +" will play first.");
-	}
-		static void populateEmptyBoard() {
-		for (int a = 0; a < 9; a++) {
-			board[a] = String.valueOf(a+1);
-		}
-	}
-	static void printBoard() {
-		System.out.println("/---|---|---\\");
-		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-		System.out.println("|-----------|");
-		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-		System.out.println("|-----------|");
-		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-		System.out.println("/---|---|---\\");
+
+
 	}
 
+
 }
+
