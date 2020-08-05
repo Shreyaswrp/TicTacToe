b67a0912bacf9e7533a070b10f19aa686b30d4cf
Shreya Swaroop <shreyaswrp@gmail.com>
2020-08-05 12:01:09 +0530

    Added:letting player and computer choose their charactes

--- TicTacToe.java
+++ TicTacToe.java
@@ -8,40 +8,77 @@ public class TicTacToe {
 	static String[] board;
 	static String turn;
 
+	// to initialize the board
+	static void populateEmptyBoard() {
+
+         	for (int a = 0; a < 9; a++) {
+                board[a] = String.valueOf(a+1);
+                }
+	}
+	//to print the board back to all empty values
+	static void printBoard() {
+
+                System.out.println("/---|---|---\\");
+                System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
+                System.out.println("|-----------|");
+                System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
+                System.out.println("/---|---|---\\");
+
+        }
+
+
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
+		Scanner	str = new Scanner(System.in);
 		board = new String[9];
-		turn =in.nextLine();
 		String winner = null;
 		populateEmptyBoard();
-
+		String playerChar = null;
+		String computerChar = null;
+		//Taking the choice of the user
+		System.out.println("Enter a number between 0 and 1");
+		int choice=in.nextInt();
+		//getting the result of the toss
 		int n=(int) (Math.random() + 0.5);
-		if(n==1) {
+		//checking if the user won the toss or not
+		if(choice==n) {
 		System.out.println("Player will proceed");
-		turn="X";
-		}else {
+		//Letting player choose a character between X and O"
+		System.out.println("Enter a character between X and O");
+		playerChar=str.nextLine();
+		String turn = "X";
+		if(playerChar.equals(turn))
+			computerChar = "O";
+		else
+			computerChar="X";
+
+		}
+		else {
+
 			System.out.println("Computer will proceed");
-			turn="O";
+			//Letting the computer choose randomly X and O
+			int result=(int) (Math.random() + 0.5);
+			if(result == 1){
+			computerChar="0";
+			playerChar="X";
+			}
+			else{
+			computerChar="X";
+			playerChar="O";
+			}
 		}
 
 		System.out.println("Welcome to 2 Player Tic Tac Toe.");
 		System.out.println("--------------------------------");
 		printBoard();
-		System.out.println(turn +" will play first.");
-	}
-		static void populateEmptyBoard() {
-		for (int a = 0; a < 9; a++) {
-			board[a] = String.valueOf(a+1);
-		}
-	}
-	static void printBoard() {
-		System.out.println("/---|---|---\\");
-		System.out.println("| " + board[0] + " | " + board[1] + " | " + board[2] + " |");
-		System.out.println("|-----------|");
-		System.out.println("| " + board[3] + " | " + board[4] + " | " + board[5] + " |");
-		System.out.println("|-----------|");
-		System.out.println("| " + board[6] + " | " + board[7] + " | " + board[8] + " |");
-		System.out.println("/---|---|---\\");
+
+
+
+		System.out.println("player character" + playerChar);
+		System.out.println("computer character" + computerChar);
+
 	}
 
 }
